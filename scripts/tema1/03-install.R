b822cf71b1977760b2bc04bbd4fea83fb904c2a1
Estebiomedico <66500970+Estebiomedico@users.noreply.github.com>
2020-06-07 18:21:57 -0400

    Update 03-install.R

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -1,5 +1,7 @@
 install.packages("tidyverse", dep = TRUE)
 
+# intalación de paquetes con dependencia
+
 library(tidyverse)
 
 install.packages("magic", dep = TRUE)
@@ -7,5 +9,11 @@ install.packages("magic", dep = TRUE)
 library(magic)
 
 magic(6)
+
+#Crea un cuadrado mágico, Un cuadrado mágico es una tabla de 
+#grado primario donde se dispone de una serie de números enteros 
+#en un cuadrado o matriz de forma tal que la suma de los números 
+#por columnas, filas y diagonales principales sea la misma
+
 
 installed.packages()
